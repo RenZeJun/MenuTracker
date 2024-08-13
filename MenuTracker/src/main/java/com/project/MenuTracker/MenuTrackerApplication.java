bf19f3e1758369c7367f7106dae8fd0c1b93c821
RenZeJun <85414115+RenZeJun@users.noreply.github.com>
2024-08-13 19:30:24 -0400

    Add files via upload

---
+++ MenuTracker/src/main/java/com/project/MenuTracker/MenuTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.project.MenuTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuTrackerApplication.class, args);
+	}
+
+}
---
+++ MenuTracker/src/test/java/com/project/MenuTracker/MenuTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.MenuTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
