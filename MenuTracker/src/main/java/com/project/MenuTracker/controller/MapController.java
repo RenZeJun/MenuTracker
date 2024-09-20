31df27604cee6a32382b44261af0643f3440c794
DESKTOP-S2SKHJS\forsk <forskyrimdemacia@gmail.com>
2024-09-20 12:24:08 -0400

    now using map service class to retrieve url in controller

--- MenuTracker/src/main/java/com/project/MenuTracker/controller/MapController.java
+++ MenuTracker/src/main/java/com/project/MenuTracker/controller/MapController.java
@@ -1,5 +1,7 @@
 package com.project.MenuTracker.controller;
 
+import com.project.MenuTracker.service.GoogleMapService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,17 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/map")
 public class MapController {
-    @Value("${map.api.key}")
-    private String apiKey;
-    @Value("${map.ids}")
-    private String mapIds;
+
+    @Autowired
+    GoogleMapService mapService;
 
     @GetMapping("/get-google-map-url")
     public ResponseEntity<String> getGoogleMap(){
-        String googleMapsApiUrl = String.format(
-                "https://maps.googleapis.com/maps/api/js?key=%s&map_ids=%s&loading=async&callback=initMap",
-                apiKey, mapIds
-        );
-        return ResponseEntity.ok(googleMapsApiUrl);
+        return ResponseEntity.ok(mapService.getGoogleMapUrl());
     }
 }
