6649aabb495ec2afb9900289a25917c1a80f43e1
DESKTOP-S2SKHJS\forsk <forskyrimdemacia@gmail.com>
2024-09-20 22:00:21 -0400

    added unit test for MapController

--- MenuTracker/src/test/java/com/project/MenuTracker/MenuTrackerApplicationTests.java
+++ MenuTracker/src/test/java/com/project/MenuTracker/MenuTrackerApplicationTests.java
@@ -1,13 +1,15 @@
-package com.project.MenuTracker;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class MenuTrackerApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.project.MenuTracker;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+//class MenuTrackerApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
